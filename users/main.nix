849d017740be45ea131225fc2ff0df0d3cc22a11
Dmitry <dlitvin3120@gmail.com>
2026-07-25 21:22:33 +0300

    Off home modules

--- users/main.nix
+++ users/main.nix
@@ -1,15 +1,15 @@
 { inputs, pkgs, ... }:
 {
-  imports = [
-    ../modules/home/core.nix
-    ../modules/home/stylix.nix
-    ../modules/home/niri.nix
-    ../modules/home/noctalia.nix
-    ../modules/home/terminal.nix
-    ../modules/home/firefox.nix
-    ../modules/home/gaming.nix
-    ../modules/home/minecraft.nix
-  ];
+  #imports = [
+  #  ../modules/home/core.nix
+  #  ../modules/home/stylix.nix
+  #  ../modules/home/niri.nix
+  #  ../modules/home/noctalia.nix
+  #  ../modules/home/terminal.nix
+  #  ../modules/home/firefox.nix
+  #  ../modules/home/gaming.nix
+  #  ../modules/home/minecraft.nix
+  #];
 
   home = {
     username = "dmitry";
