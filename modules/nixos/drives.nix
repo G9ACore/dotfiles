f0bc35010a997cbf56ff70f1785b1c8fc93fdd38
G9ACore <dlitvin3120@gmail.com>
2026-08-14 23:57:34 +0300

    Add additional disks support

--- modules/nixos/drives.nix
+++ modules/nixos/drives.nix
@@ -1,19 +1,37 @@
-{
-  fileSystems."/home/dmitry/drives/gaming" = {
-    device = "/dev/disk/by-uuid/149424719424580E";
-    fsType = "ntfs3";
-    options = [
-      "rw"
-      "uid=1000"
-      "gid=1000"
-      "umask=000"
-      "user"
-      "exec"
-      "nofail"
-      "noauto"
-      "x-systemd.automount" # монтировать при первом обращении к папке
-      "x-systemd.device-timeout=5" # ждать устройство максимум 5 с, а не 90
-      "x-systemd.idle-timeout=120" # отмонтировать через 2 мин простоя (опционально)
-    ];
+{lib, ...}: let
+  mkDrive = {
+    uuid,
+    # mountPoint,
+    fsType ? "ntfs3",
+    extraOptions ? [],
+  }: {
+    device = "/dev/disk/by-uuid/${uuid}";
+    inherit fsType;
+    options =
+      [
+        "rw" "user" "exec" "nofail" "noauto"
+        "x-systemd.automount"
+        "x-systemd.device-timeout=5"
+        "x-systemd.idle-timeout=120"
+      ]
+      ++ (
+        if builtins.elem fsType ["ntfs3" "exfat" "vfat"]
+        then ["uid=1000" "gid=1000" "umask=000"]
+        else []
+      )
+      ++ extraOptions;
+  };
+in {
+  # Чтобы искать диски -> lsblk -f
+  fileSystems = {
+    "/home/dmitry/drives/games" = mkDrive {
+      uuid = "149424719424580E";
+    };
+    "/home/dmitry/drives/data" = mkDrive {
+      uuid = "DE761D31761D0C41";
+    };
+    "/home/dmitry/drives/extra" = mkDrive {
+      uuid = "8A7811DB7811C6BB ";
+    };
   };
 }
